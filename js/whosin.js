37a2ce2159253f1b86d7eeb4f8f9c438a1f7fc9d
Omid <omonshizadeh@linkedin.com>
2012-04-20 11:04:28 -0700

    Adding the iframe

--- js/whosin.js
+++ js/whosin.js
@@ -6,6 +6,7 @@
     inScript = null,
     inInner,
     whosInLayout,
+    noProfileImage = 'http://localhost/whosin/img/icon_no_photo_no_border_60x60.png',
     walker = document.createTreeWalker(document.body,NodeFilter.SHOW_TEXT,null,false);
     /*
     function walk() {
@@ -17,14 +18,23 @@
     }
     */
 
+    function createTheIFrame () {
+      var iframe = document.createElement('iframe');
+      iframe.src = 'http://localhost/whosin';
+      iframe.id = 'whosinIframe';
+      body.appendChild(iframe);
+    }
+
     function initINFramework() {
       if(!body) {
+
         inStyles = document.createElement('link');
         inStyles.rel = 'stylesheet';
         inStyles.type = 'text/css';
-        inStyles.href = 'http://localhost/css/whosin.css';
+        inStyles.href = 'http://localhost/whosin/css/whosin.css';
 
         body = document.getElementsByTagName('body')[0];
+        /*
         inScript = document.createElement('script');
         inScript.type = 'text/javascript';
         inInner = 'api_key: x2tkqnnvqo4d\n\
@@ -32,8 +42,10 @@
                    authorize: true';
         inScript.innerHTML = inInner;
         inScript.src = 'http://platform.linkedin.com/in.js';
+        */
         body.appendChild(inStyles);
-        body.appendChild(inScript);
+        //body.appendChild(inScript);
+        createTheIFrame();
       }
     }
 
@@ -64,6 +76,7 @@
 
     function doSearch() {
       var peopleDiv = '<ul>',
+          profile,
           i;
       if(IN.User.isAuthorized()){
         createLayout();
@@ -76,16 +89,18 @@
           .result(function(res){
             if(res){
               for(i = 0, len = res.people._count; i < len; i++){
+                profile = res.people.values[i];
                 peopleDiv += '<li>' + 
-                                '<img src="' + res.people.values[i].pictureUrl + '" />' +
-                                '<h2><a href="' + res.people.values[i].publicProfileUrl + '" target="_blank">'  + res.people.values[i].firstName + ' ' + res.people.values[i].lastName + '</a></h2>' +
-                                '<h3>' + res.people.values[i].headline + '</h3>' +
+                                '<img src="' + (profile.pictureUrl || noProfileImage) + '" />' +
+                                '<h2><a href="' + profile.publicProfileUrl + '" target="_blank">'  + profile.firstName + ' ' + profile.lastName + '</a></h2>' +
+                                '<h3>' + profile.headline + '</h3>' +
                               '</li>';
               }
 
               peopleDiv += '</ul>';
 
-              whosInLayout.innerHTML = '<h1>' + res.numResults + ' record(s) found!</h1>' + peopleDiv;
+              whosInLayout.innerHTML = '<a href="#" class="close-button">close</a><h1>' + res.numResults + ' record(s) found!</h1>' + peopleDiv;
+              IN.Util.removeClass(whosInLayout, 'hide-module');
             }
           }
         );
@@ -96,12 +111,20 @@
       
     }
 
+    function onClick(e) {
+      if(e.target.className === 'close-button') {
+        e.preventDefault();
+        IN.Util.addClass(whosInLayout, 'hide-module');
+      }
+    }
+
     function createLayout(){
       if(!whosInLayout){
         whosInLayout = document.createElement('div');
         whosInLayout.setAttribute('id', 'whosInWrapper');
         whosInLayout.style.height = window.innerHeight + 'px';
         body.appendChild(whosInLayout);
+        IN.Event.on(whosInLayout, 'click', onClick, null);
       }
     }
 
